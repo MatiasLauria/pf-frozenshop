56e8b9f6ab8f1b71750be9177badfde6d7c5ceef
Matias Lauria <matiaslauria87@gmail.com>
2022-11-22 16:43:39 -0300

    form-firebase-terminado

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,116 +1,99 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import { useCartContext } from '../../Context/CartContext'
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCartContext } from '../../Context/CartContext';
 import ItemCart from '../ItemCart/ItemCart';
-import { addDoc,collection, getFirestore } from 'firebase/firestore'
-import './Cart.css'
+import './Cart.css';
 
-
-
-
+const emptyUser = {
+    nombre: '',
+    apellido: '',
+    email: '',
+};
 
 const Cart = () => {
+    const navigate = useNavigate();
+    const { cart, totalPrice, clearCart } = useCartContext();
 
-  const { cart, totalPrice,} = useCartContext();
+    const [user, setUser] = useState(emptyUser);
 
+    const capturarImputs = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value });
+    };
 
+    const handleClick = () => {
+        const order = {
+            buyer: user,
+            items: cart.map((product) => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+            total: totalPrice(),
+        };
 
-  const order = {
-    buyer: '',
-    items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
-    total: totalPrice(),
-  }
-  
-  
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order).then(alert('Su orden se realizo con exito')).then(clearCart).then(navigate('/'));
+    };
 
-  const valorInicial ={
-    nombre:'',
-    apellido:'',
-    email:''
-}
+    if (cart.length === 0) {
+        return (
+            <>
+                <div style={{ textAlign: 'center' }}>
+                    <p>No hay productos en el carrito</p>
+                    <button className="returnHome">
+                        <Link to="/">Volver a comprar</Link>
+                    </button>
+                </div>
+            </>
+        );
+    }
 
-  const [user, setUser] = useState(valorInicial)
+    return (
+        <>
+            {cart.map((product) => (
+                <ItemCart
+                    key={product.id}
+                    product={product}
+                />
+            ))}
 
-  const capturarImputs = (e) =>{
-    const {name, value} = e.target;
-    setUser({...user, [name] : value})
-  }
+            <div style={{ textAlign: 'center' }}>
+                <p>TOTAL: {totalPrice()}</p>
+            </div>
 
-  const guardarDatos = async(e) =>{
-    e.preventDefault();
-    setUser({...valorInicial})
-  }
+            <div style={{ textAlign: 'center' }}>
+                <input
+                    type="text"
+                    name="nombre"
+                    placeholder="Nombre"
+                    onChange={capturarImputs}
+                    value={user.nombre}
+                />
 
-  const handleClick = () => {
-    const db = getFirestore();
-    const ordersCollection = collection (db, 'orders');
-    addDoc(ordersCollection, order)
-    .then(alert('Su orden se realizo con exito'))
-}
+                <input
+                    type="text"
+                    name="apellido"
+                    placeholder="Apellido"
+                    onChange={capturarImputs}
+                    value={user.apellido}
+                />
 
-   
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    onChange={capturarImputs}
+                    value={user.email}
+                />
 
-  if (cart.length === 0){
-    return(
-      <>
-      <div style={{ textAlign: 'center' }}>
-      <p>No hay productos en el carrito</p>
-      <button className='returnHome'><Link to='/'>Volver a comprar</Link></button>
-      </div>
-      </>
-    )
-  }
+                <button
+                    className="checkOut"
+                    onClick={handleClick}
+                >
+                    Finalizar compra
+                </button>
+            </div>
+        </>
+    );
+};
 
-      
-
-  return (
-  
-
-    <>
-      {
-        cart.map(product => <ItemCart key={product.id} product={product}/>)
-      }
-
-      <div style={{ textAlign: 'center' }}>
-        <p>
-          TOTAL: {totalPrice()}
-        </p>
-
-        
-        
-      
-      </div>
-
-
-      <div style={{ textAlign: 'center' }}>
-      <form onSubmit={guardarDatos}>
-          <input type='text' name='nombre' placeholder='Nombre' 
-          onChange={capturarImputs} value={user.nombre}/>
-         
-         <input type='text' name='apellido' placeholder='Apellido'
-          onChange={capturarImputs} value={user.apellido}/>
-         
-          <input type='text' name='email' placeholder='Email'
-          onChange={capturarImputs} value={user.email}/> 
-
-          <button className='checkOut'><Link to='/' onClick={handleClick}> Finalizar compra</Link></button>
-
-                   
-      </form>
-      </div>
-      </>
-      
-)
- 
-
-
-
-}
-
-
-
-
-
-
-
-export default Cart
+export default Cart;
